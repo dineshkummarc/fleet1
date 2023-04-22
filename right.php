5f6929a2b279121a0c3fa3ec5e81cee43987f076
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:13 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,8 @@
+<?php
+
+$this->add_right("fleet", "fleet", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("fleet", "fleet", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("fleet", "fleet", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("fleet", "fleet", "staff", view:true, add:true, edit:true);
+$this->add_right("fleet", "fleet", "registered", view:true, add:true);
+$this->add_right("fleet", "fleet", "guest", view:true, );
