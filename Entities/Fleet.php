bf6a603d24833239673033263fabab3d6131c9d0
Dedan <dedanirungu@gmail.com>
2024-04-12 10:00:03 +0300

    Add rights method to Fleet.php

--- Entities/Fleet.php
+++ Entities/Fleet.php
@@ -61,4 +61,15 @@ class Fleet extends BaseModel
 
         return $structure;
     }
+
+
+    /**
+     * Define rights for this model.
+     *
+     * @return array
+     */
+    public function rights(): array
+    {
+
+    }
 }
