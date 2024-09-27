cf5440af7187f8d8a3d4d3c0f68f018d70c01d42
Dedan <dedanirungu@gmail.com>
2024-09-27 10:06:20 +0300

    Changes to files: database/migrations/2024_08_03_081524_create_fleet_fleet_table.php database/migrations/2024_09_03_133114_add_deleted_at_to_fleet_fleet_table.php

--- database/migrations/2024_08_03_081524_create_fleet_fleet_table.php
+++ database/migrations/2024_08_03_081524_create_fleet_fleet_table.php
@@ -18,7 +18,12 @@ return new class extends Migration
             $table->string('description')->nullable();
             $table->tinyInteger('published')->default(0);
 
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->onDelete('set null');
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
