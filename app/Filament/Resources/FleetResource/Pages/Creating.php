a6498cc7fc1b3546a926ff8a10c656f8231e4d3a
Dedan <dedanirungu@gmail.com>
2024-10-14 00:07:27 +0300

    Changes to files: app/Filament/Resources/FleetResource.php app/Filament/Resources/FleetResource/Pages/Creating.php app/Filament/Resources/FleetResource/Pages/Editing.php app/Filament/Resources/FleetResource/Pages/Listing.php

--- app/Filament/Resources/FleetResource/Pages/Creating.php
+++ app/Filament/Resources/FleetResource/Pages/Creating.php
@@ -3,8 +3,10 @@
 namespace Modules\Fleet\Filament\Resources\FleetResource\Pages;
 
 use Modules\Base\Filament\Resources\Pages\CreatingBase;
+use Modules\Fleet\Filament\Resources\FleetResource;
 
 // Class List that extends ListBase
 class Creating extends CreatingBase
 {
+    protected static string $resource = FleetResource::class;
 }
--- app/Filament/Resources/FleetResource/Pages/Editing.php
+++ app/Filament/Resources/FleetResource/Pages/Editing.php
@@ -3,8 +3,10 @@
 namespace Modules\Fleet\Filament\Resources\FleetResource\Pages;
 
 use Modules\Base\Filament\Resources\Pages\EditingBase;
+use Modules\Fleet\Filament\Resources\FleetResource;
 
 // Class List that extends ListBase
 class Editing extends EditingBase
 {
+    protected static string $resource = FleetResource::class;
 }
