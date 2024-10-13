71206b2e2ca23252aab4bafab57959a199f93474
Dedan <dedanirungu@gmail.com>
2024-10-13 16:28:56 +0300

    Release 2024.12

---
+++ app/Filament/Resources/FleetResource/Pages/Creating.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Fleet\Filament\Resources\FleetResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\CreatingBase;
+
+// Class List that extends ListBase
+class Creating extends CreatingBase
+{
+}
---
+++ app/Filament/Resources/FleetResource/Pages/Editing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Fleet\Filament\Resources\FleetResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\EditingBase;
+
+// Class List that extends ListBase
+class Editing extends EditingBase
+{
+}
